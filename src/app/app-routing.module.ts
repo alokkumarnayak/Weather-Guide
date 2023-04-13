08aa57fd534a3dac7115452f67ab14a220827add
alokkumarnayak <alokkumar.nayak@robosoftin.com>
2023-04-13 19:18:02 +0530

    Lazy loading implemented some modules removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeWeatherComponent } from 'src/app/home-weather/home-weather.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', loadChildren: () => import('src/app/home-weather/home-weather.module').then(m => m.HomeWeatherModule) }];
+  { path: '', pathMatch: 'full', loadChildren: () => import('./core/home-weather/home-weather.module').then(m => m.HomeWeatherModule) }];
+  
   
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
